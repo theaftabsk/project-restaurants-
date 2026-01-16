15f162901bee7c0e1b66fa41812a876c86420866
theaftabsk <165376380+theaftabsk@users.noreply.github.com>
2026-01-16 16:27:01 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$user = "u849033904_aftabsk0005";
-$pass = "!2Jk*GnKREb";
-$db   = "u849033904_aftabsk0005";
+$user = "xxxxxxxxxxx";
+$pass = "xxxxxxxxxxx";
+$db   = "xxxxxxxxxxx";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
@@ -10,3 +10,4 @@ if (!$conn) {
     die("Database Connection Failed");
 }
 ?>
+
